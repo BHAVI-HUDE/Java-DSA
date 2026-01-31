7181477c8bc9fde0bfcd7403465a59a046cebacc
BHAVI HUDE <145373300+BHAVI-HUDE@users.noreply.github.com>
2026-01-31 18:32:20 +0530

    Add comments for Activity Selection explanation
    Added comments to explain the Activity Selection problem.

--- GreedyAlgorithms/ActivitySelection.java
+++ GreedyAlgorithms/ActivitySelection.java
@@ -4,6 +4,16 @@ import java.util.*;
 
 public class ActivitySelection {
     public static void main(String[] args) {
+
+        // Activity Selection
+        
+        // You are given n activities with their start and end times. Select the maximum number of activities
+        // that can be performed by a single person, assuming that a person can only work on a single
+        // activity at a time. Activities are sorted according to end time.
+        // start = [10, 12, 20]
+        // end = [20, 25, 30]
+        // ans = 2 (AO & A2)
+                
         int start[] = {1,3,0,5,8,5};
         int end[] = {2,4,6,7,9,9};
 
